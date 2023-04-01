353e074ab4021484a02339be4133dc30d9c5c2c1
EGSTAD <jordanegstad@gmail.com>
2023-04-01 11:39:26 -0500

    experimenting with p5

--- components/visualizations/rose.client.vue
+++ components/visualizations/rose.client.vue
@@ -1,57 +1,65 @@
 <template>
   <div class="p5">
-    <div class="p5__content" :id="p5CanvasID"></div>
+    <div class="p5__content" ref="p5Canvas"></div>
   </div>
 </template>
 
 <script setup>
-import { v4 as uuidv4 } from "uuid";
-import { ref, onMounted, watch, onUnmounted } from "vue";
+import { ref, onMounted, watch, onUnmounted, nextTick } from "vue";
 import { storeToRefs } from "pinia";
 import { loadP5 } from "@/assets/scripts/utilLoadFile";
 import { useLibraryStore } from "@/stores/libraries";
 const p5Canvas = ref();
 const p5Instance = ref(null);
 const { p5HasLoaded } = storeToRefs(useLibraryStore());
-const p5CanvasID = uuidv4();
 
 loadP5();
 
 const p5Init = () => {
   let sketch = function (p) {
-    let x = 100;
-    let y = 100;
+    const rect = p5Canvas.value.getBoundingClientRect();
+    let x = rect.width * 0.5;
+    let y = rect.height * 0.5;
 
     p.setup = function () {
-      const renderer = p.createCanvas(200, 200);
-      renderer.parent(p5CanvasID);
+      p.createCanvas(rect.width, rect.height);
     };
 
     p.draw = function () {
       p.background(50);
+      p.rectMode(p.CENTER);
       p.fill(255);
+      p.rotate(p.frameCount);
       p.rect(x, y, 50, 50);
     };
   };
 
-  p5Instance.value = new window.p5(sketch);
+  nextTick(() => {
+    p5Instance.value = new window.p5(sketch, p5Canvas.value);
+  });
+  // p5Instance.value.style.visibility = "visible";
 };
 
 watch(p5HasLoaded, (time) => {
-  if (p5HasLoaded.value && !p5Instance.value) p5Init();
+  nextTick(() => {
+    if (p5HasLoaded.value && !p5Instance.value) p5Init();
+  });
 });
 
 onMounted(() => {
-  if (p5HasLoaded.value && !p5Instance.value) p5Init();
+  nextTick(() => {
+    if (p5HasLoaded.value && !p5Instance.value) p5Init();
+  });
 });
 
 onUnmounted(() => p5Instance.value.remove());
 </script>
 
-<style scoped>
+<style>
 .p5 {
   display: flex;
-  width: 100%;
+  flex: 1;
+  width: 100vw;
   height: 100%;
 }
 
